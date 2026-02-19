e47d431197535410600de39ff64372da458f964f
RommelYmrelMapalo <rmmapalo2632ant@student.fatima.edu.ph>
2026-02-19 23:37:54 +0800

    test

--- website/__init__.py
+++ website/__init__.py
@@ -2,14 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'jay'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['UPLOAD_FOLDER'] = 'website/static/uploads'
 
     db.init_app(app)
